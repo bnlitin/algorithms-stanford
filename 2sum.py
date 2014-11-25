b2dc13d77477099b283fee791d4e4dd9e626465f
Boris Litinsky <bnlitin@yahoo.com>
2014-11-25 15:46:08 -0800

    Initial check in

---
+++ 2sum.py
@@ -0,0 +1,30 @@
+
+def loadDiction(infile):    
+    with open(infile, 'r') as f:
+        diction = set()
+        for line in f:
+            num = int(line.strip())
+            diction.add(num)
+    return diction
+
+def twoSum(diction):
+    matches = 0
+    for t in range(-10000,10001,1):
+        for x in diction:
+            y = t-x
+            if x != y and y in diction:
+                matches += 1
+                print "total matches=",matches," with ","x=",x,"y=",y,"t=",t
+                break
+            
+    print "total matches=",matches     
+    return matches
+
+# total matches= 427
+def main():
+    diction = loadDiction('algo1-programming_prob-2sum.txt')   
+    twoSum(diction)
+
+    
+if __name__ == "__main__":
+    main()
